572c951763771eb9de5fb38cb5a6be71b57a10b1
Abdul Haqani <ahaqa011@uottawa.ca>
2019-07-04 16:53:33 -0400

    created auth and passport routes

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://AbdulHaqani:Dudeperfect1@storybooks-dev-muq07.mongodb.net/test?retryWrites=true&w=majority',
+  googleClientID:
+    '571248621265-t651acdeo6k3ocgsukjo91rjkuj08fig.apps.googleusercontent.com',
+  googleClientSecret: 'tv_JM4ZmgMUba1wOLjX198JJ',
+};
