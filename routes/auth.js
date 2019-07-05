ca58a6fb695e91ab7a3133fe288dc1da5941bd1d
Abdul Haqani <ahaqa011@uottawa.ca>
2019-07-05 00:11:49 -0400

    fixed scope

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,10 @@ router.get(
 
 router.get(
   '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/', scope: 'https://www.googleapis.com/auth/plus.login' }),
+  passport.authenticate('google', {
+    failureRedirect: '/',
+    scope: 'https://www.googleapis.com/auth/plus.login',
+  }),
   (req, res) => {
     res.redirect('/dashboard');
   }
