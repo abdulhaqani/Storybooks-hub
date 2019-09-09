4e3e7705dbd273d5b867371fcce7545bfee1037f
Abdul Haqani <ahaqa011@uottawa.ca>
2019-09-09 16:17:35 -0400

    user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+// Create schema
+const UserSchema = new Schema({
+  googleID: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  LastName: {
+    type: String,
+  },
+});
