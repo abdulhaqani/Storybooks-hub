cf236cfafac9c563c8553e67c84d131cddb8924b
Abdul Haqani <ahaqa011@uottawa.ca>
2019-12-31 13:19:49 -0500

    dashboard created

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+const Story = mongoose.model('stories');
 const router = express.Router();
 const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
 
@@ -8,7 +10,11 @@ router.get('/', ensureGuest, (req, res) => {
 });
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-  res.render('index/dashboard');
+  Story.find({ user: req.user.id }).then(stories => {
+    res.render('index/dashboard', {
+      stories,
+    });
+  });
 });
 
 router.get('/about', (req, res) => {
