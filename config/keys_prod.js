0d0ee6570479a8972dd0dfda6283003af2ee3222
Abdul Haqani <ahaqa011@uottawa.ca>
2020-01-02 01:44:33 -0500

    final push

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,5 @@
 module.exports = {
-  // 'mongodb://localhost/storybooks-dev',
   mongoURI: process.env.MONGO_URI,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 };
-// test
